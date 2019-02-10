461a282a0ea7e8f3ae46693b6e08d55c4b4962ff
pepper5319 <pepper5319@gmail.com>
2019-02-10 14:17:59 -0500

    Added React Navigation

--- App.js
+++ App.js
@@ -10,50 +10,22 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { connect } from 'react-redux';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import HomeScreen from './pages/HomeScreen.js';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+
+const AppNavigator = createStackNavigator({
+  Home:HomeScreen
 });
+const AppContainer = createAppContainer(AppNavigator);
 
-type Props = {};
-class App extends Component<Props> {
+class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const mapStateToProps = state => ({});
 
-const mapStateToProps = state => ({
-
-});
-
-//#endregion
 
 export default connect(mapStateToProps, {  })(App);
